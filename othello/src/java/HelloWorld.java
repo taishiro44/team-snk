210254c312c9d152ff83a8a6977709392805b5c7
tanaka taishiro <taishiro.tanaka@minnie.ai.kyutech.ac.jp>
2017-07-18 15:19:20 +0900

    サーブレットの練習 クリックすると、helloworldを表示する。

---
+++ othello/src/java/HelloWorld.java
@@ -0,0 +1,29 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
